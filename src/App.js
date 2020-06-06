57697d696becb53105c9555604d54f710b828fb1
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-06-06 15:12:48 +0300

    routes added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import Header from './components/Header'
 import AddRoutine from './components/AddRoutine'
 import Routines from './components/Routines'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import About from './components/About';
+
 function App() {
   return (
-    <div className="App">
+    <div className="relative pb-10 min-h-screen">
+      <Router>
         <Header />
         <AddRoutine />
-        <Routines />
+        <Switch>
+          <Route exact path="/">
+            <Routines />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+        
     </div>
   );
 }
